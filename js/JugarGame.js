2111a9166bc4a0b6f780fe3272b1c81d4cc3225e
Jerry <gmanuel@trigarante.com>
2019-09-03 10:08:05 -0500

    rectangulos over

--- js/JugarGame.js
+++ js/JugarGame.js
@@ -1,15 +1,18 @@
+var MONTO_DIAMANTES = 20;
 GamePlayManager = {
     init: function() {
-        juego.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        juego.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; // resize
         juego.scale.pageAlignHorizontally = true;
         juego.scale.pageAlignVertically = true;
-        this.flagFirstMouseDown = false;
+        this.flagFirstMouseDown = false; // click
     },
     preload: function() {
         juego.load.image(
             'background', 'assets/images/background.png');
         juego.load.spritesheet(
             'horse', 'assets/images/horse.png', 84, 156, 2); // wi, he, 2img
+        juego.load.spritesheet(
+            'diamante', 'assets/images/diamonds.png', 81, 84, 4)
     },
     create: function() {
         juego.add.sprite(0, 0, 'background');
@@ -19,9 +22,39 @@ GamePlayManager = {
         this.caballito.y = juego.height/2; // posicion y
         this.caballito.anchor.setTo(0.5); // (x, y) cambia el centro
         juego.input.onDown.add(this.onTap, this); // activa cursor
+
+        this.diamantes = [];
+        for(var i=0; i<MONTO_DIAMANTES; i++) {
+            var diamante = juego.add.sprite(10, 10 , 'diamante');
+            diamante.frame = juego.rnd.integerInRange(0, 3);
+            // aumenta tamaño y disminuye
+            diamante.scale.setTo(0.30 + juego.rnd.frac()); 
+            diamante.anchor.setTo(0.5);
+            diamante.x = juego.rnd.integerInRange(50, 1050);
+            diamante.y = juego.rnd.integerInRange(50, 600);
+            this.diamantes[i] = diamante;
+
+        }
         // this.caballito.angle = 90; // rotacion
         // this.caballito.scale.setTo(4); // tamaño
         // this.caballito.alpha = 1; // (0,1) opacidad
+    },
+    boundsDiamantes: function(dActual) { // limites
+        return new Phaser.Rectangle(
+            dActual.left,
+            dActual.top,
+            dActual.width,
+            dActual.height
+        )
+    },
+    rectanguloSuperpuesto: function(rec1, rec2) {
+        if(rec1.x > rec2.x + rec2.width || rec2.x > rec1.x + rec1.width) {
+            return false;
+        }
+        if(rec1.y > rec2.y + rec2.width || rec2.y > rec1.y + rec1.width) {
+            return false;
+        }
+        return true;
     },
     onTap: function() {
         this.flagFirstMouseDown = true;
